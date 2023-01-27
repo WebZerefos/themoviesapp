0ee6288a715762846fdba0c3fea7d4ea4c72b5d2
WebZerefos <anything.codable@gmail.com>
2023-01-27 16:20:11 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text className="text-2xl">Movies</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
